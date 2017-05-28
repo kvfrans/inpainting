db743456247260a40f60af810f368556c1516d0d
Kevin Frans <kevin@bobthechicken.com>
2017-05-27 20:07:23 -0700

    update

---
+++ ops.py
@@ -0,0 +1,57 @@
+import numpy as np
+import tensorflow as tf
+
+class batch_norm(object):
+            # h1 = lrelu(tf.contrib.layers.batch_norm(conv2d(h0, self.df_dim*2, name='d_h1_conv'),decay=0.9,updates_collections=None,epsilon=0.00001,scale=True,scope="d_h1_conv"))
+    def __init__(self, epsilon=1e-5, momentum = 0.9, name="batch_norm"):
+        with tf.variable_scope(name):
+            self.epsilon = epsilon
+            self.momentum = momentum
+            self.name = name
+
+    def __call__(self, x, train=True):
+        return tf.contrib.layers.batch_norm(x, decay=self.momentum, updates_collections=None, epsilon=self.epsilon, scale=True, scope=self.name)
+
+# standard convolution layer
+def conv2d(x, inputFeatures, outputFeatures, name):
+    with tf.variable_scope(name):
+        w = tf.get_variable("w",[5,5,inputFeatures, outputFeatures], initializer=tf.truncated_normal_initializer(stddev=0.02))
+        b = tf.get_variable("b",[outputFeatures], initializer=tf.constant_initializer(0.0))
+        conv = tf.nn.conv2d(x, w, strides=[1,2,2,1], padding="SAME") + b
+        return conv
+
+def conv_transpose(x, outputShape, name):
+    with tf.variable_scope(name):
+        # h, w, out, in
+        w = tf.get_variable("w",[5,5, outputShape[-1], x.get_shape()[-1]], initializer=tf.truncated_normal_initializer(stddev=0.02))
+        b = tf.get_variable("b",[outputShape[-1]], initializer=tf.constant_initializer(0.0))
+        convt = tf.nn.conv2d_transpose(x, w, output_shape=outputShape, strides=[1,2,2,1])
+        return convt
+
+# leaky reLu unit
+def lrelu(x, leak=0.2, name="lrelu"):
+    with tf.variable_scope(name):
+        f1 = 0.5 * (1 + leak)
+        f2 = 0.5 * (1 - leak)
+        return f1 * x + f2 * abs(x)
+
+# fully-conected layer
+def dense(x, inputFeatures, outputFeatures, scope=None, with_w=False):
+    with tf.variable_scope(scope or "Linear"):
+        matrix = tf.get_variable("Matrix", [inputFeatures, outputFeatures], tf.float32, tf.random_normal_initializer(stddev=0.02))
+        bias = tf.get_variable("bias", [outputFeatures], initializer=tf.constant_initializer(0.0))
+        if with_w:
+            return tf.matmul(x, matrix) + bias, matrix, bias
+        else:
+            return tf.matmul(x, matrix) + bias
+
+def merge(images, size):
+    h, w = images.shape[1], images.shape[2]
+    img = np.zeros((h * size[0], w * size[1]))
+
+    for idx, image in enumerate(images):
+        i = idx % size[1]
+        j = idx / size[1]
+        img[j*h:j*h+h, i*w:i*w+w] = image
+
+    return img
